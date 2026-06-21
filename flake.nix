2c30fb6ad90538dccd145a04734c78da1dbe72d7
RyosukeDTomita <d310151c2@gmail.com>
2026-06-21 22:55:11 +0900

    ci: test は Haskell 関連変更時のみ実行(path フィルター)、静的ビルドのソースを絞る、URL を acac-cli に更新

--- flake.nix
+++ flake.nix
@@ -24,16 +24,28 @@
         unstablePkgs = import nixpkgs-unstable { inherit system; };
         hpkgs = pkgs.haskell.packages.ghc9122;
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        # cabal パッケージのソースは Haskell 関連ファイルだけに限定する。
+        # README/npm/docs/flake 等を変えてもビルドのハッシュが変わらず、
+        # cachix のキャッシュ(静的ビルド)が効き続けるようにするため。
+        acacSrc = pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = pkgs.lib.fileset.unions [
+            ./acac.cabal
+            ./src
+            ./app
+            ./test
+          ];
+        };
       in
       {
         formatter = treefmtEval.config.build.wrapper;
 
         # `nix build` で acac の実行バイナリを生成する(開発・動作確認用、nix store に動的リンク)。
-        packages.default = hpkgs.callCabal2nix "acac" ./. { };
+        packages.default = hpkgs.callCabal2nix "acac" acacSrc { };
 
         # `nix build .#static` で配布用の musl 静的バイナリを生成する(GitHub Releases へ載せる)。
         packages.static = pkgs.haskell.lib.justStaticExecutables (
-          pkgs.pkgsStatic.haskell.packages.ghc9122.callCabal2nix "acac" ./. { }
+          pkgs.pkgsStatic.haskell.packages.ghc9122.callCabal2nix "acac" acacSrc { }
         );
 
         devShells.default = pkgs.mkShell {
